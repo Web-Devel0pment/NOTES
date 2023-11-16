f5e8cca8c5cdad2e61ca4800323e6602d83baa77
mani1916 <lmanikandandan031@gmail.com>
2023-11-16 22:05:27 +0530

    day2

---
+++ index.js
@@ -0,0 +1,175 @@
+// // //use key word and declartion
+// // //keyword let,var,const
+// // //initialize variable
+// var name1;
+
+// const aadhar=987654321;
+// name1="avinaash"+1;
+// console.log(name1)
+// name1=99
+// c=true
+// console.log(c)
+
+
+///arithmetix expression
+//operand and operators
+//operands=93,398,933
+//operator=+,-,/,*,%
+//bitwise operator=&,|,^,~
+//assignment opertors= +=,-+,*=,/=,%=,++,--
+//expression=left to right
+//BODMAS
+// console.log(3+8)
+// let x=90,y=89;
+// x+=y
+
+// // x=3+2*(8*10)
+// // //3+2*80
+// // //3+160
+// // //163
+// // console.log(x)
+
+// // user input
+// // // var x=window.prompt("enter your age")
+// // x=Number(x)
+
+// // console.log(x)
+// b=98
+// b+=1
+// console.log(typeof b,b)
+// b=String(b)
+// b+=1
+// console.log(typeof b,b)
+// x=false
+// b=""
+// z=Boolean(b)
+
+// console.log(typeof x,z,b)
+// //area of cirlce
+
+// // var btn=document.getElementById("btn1")
+// // btn.onclick=()=>x("mani")
+
+// // function x(name){
+// //     var msg=document.getElementById("id1").value
+// //     console.log(msg)
+// //     document.getElementById("man").innerHTML=msg
+// // }
+
+//MAth
+
+// x=4
+// console.log(Math.round(x))
+// console.log(Math.floor(x))
+// console.log(Math.ceil(x))
+// console.log(Math.pow(3,3))
+// console.log(Math.sqrt(x))
+// console.log(Math.sqrt(x))
+// console.log(Math.sign(x))
+// console.log(Math.max(999,99999))
+// console.log(Math.min(999,99999))
+
+// num=99
+// console.log(num%2)
+// if(num%2==0)
+// console.log("even")
+// else
+
+///var=(condition)?"":""
+// x=99
+// y=90
+// var ans=(x>y)?y:x
+// console.log(ans)
+/*
+Task  - 1
+Valid Triangle
+*/
+
+// var btn=document.getElementById("check");
+// var ans=document.getElementById("ans");
+// btn.onclick=function () {
+    
+// var a=document.getElementById("side1").value;
+// var b=document.getElementById("side2").value;
+// var c=document.getElementById("side3").value;
+//     a=Number(a)
+//     b=Number(b)
+//     c=Number(c)
+//     var x=a+b;
+//     var y=b+c;
+//     var z=a+c;
+//     console.log(y,a,typeof a);
+//     console.log(z,b);
+//     console.log(x,c);
+//     if(y>a && z>b && x>c)
+//     ans.innerHTML="Hey! its Valid Triangle ✌️"
+//     else
+//     ans.innerHTML="Nope! its InValid Triangle 😒"
+// }
+/*
+var nam="cenzo zone"
+
+console.log(nam.length)
+console.log(nam.charAt(nam.length-1))
+console.log(nam.indexOf('z',4))
+console.log(nam.trim())
+console.log(nam.toUpperCase())
+nam=nam.replaceAll("-","*")
+console.log(nam.toUpperCase())
+console.log(nam.substring(2,5)
+*/
+
+// var sli="cenzo zone"
+// console.log(sli)
+// var ans1=sli.slice(3,7);
+//                 //0        1         2       3        4
+//                 //-5     -4         -3        -2          -1 
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const myBest = fruits.slice(0,4);
+// console.log(myBest)
+
+
+//Method chaining
+
+// var name="avinash".toUpperCase().toLowerCase().slice(2).toUpperCase()
+// console.log(name)
+
+//if
+//if - else
+//if if 
+//elseif if ladder
+// var x=0;
+// var y=2;
+// var z=3;
+// if(!(x==1))
+// console.log("noo")
+// else
+// console.log("nooqqq")
+
+// // if(x===0)
+// // console.log("noo")
+// // else if(y==0)
+// // console.log("nooty")
+// // else if(z==0)
+// // console.log("nooz")
+// // else
+// // console.log("xyz")
+
+//4%5
+
+//switch
+// var x='9';
+// var a=9;
+// var b=5;
+// switch(x){
+//     case '+':console.log(a+b);
+//     break;
+//     case '-':console.log(a-b);
+//     break;
+//     case '*':console.log(a*b);
+//     break;
+//     case '/':console.log(a/b);
+//     break;
+//     default:
+//         console.log(a%b)
+// }
